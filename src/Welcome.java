68ff333deea3991cf503a6835399a2c720bf395e
kkakaroTT <162276812+dwssd1250@users.noreply.github.com>
2024-03-12 16:22:57 +0900

    level2 finish (edit)
    level2 finish (edit)

--- src/Welcome.java
+++ src/Welcome.java
@@ -13,8 +13,8 @@ public class Welcome {
 		
 		System.out.println("1. 고객 정보 확인하기\t5. 장바구니에 항복 추가하기");
 		System.out.println("2. 장바구니 상품 목록 보기\t6. 장바구니의 항목 수량 줄이기");
-		System.out.println("3. 장바구니 비우기\t7. 장바구니의 항목 삭제하기");
-		System.out.println("4. 영수증 표시하기\t8. 종료");
+		System.out.println("3. 장바구니 비우기\t\t7. 장바구니의 항목 삭제하기");
+		System.out.println("4. 영수증 표시하기\t\t8. 종료");
 		System.out.println("****************************************");
 	}
 
