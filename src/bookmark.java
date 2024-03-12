95f7ac359e54b705b6600f3252d9f56e1cd2382a
kkakaroTT <162276812+dwssd1250@users.noreply.github.com>
2024-03-12 14:40:55 +0900

    level1 finish!
    level1 finish!

---
+++ src/bookmark.java
@@ -0,0 +1,12 @@
+
+public class bookmark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to shopping mall!");
+		System.out.println("Welcome to bookmark!");
+		
+
+	}
+
+}
