a1f20068101a3d166e5ec2247d25ee7585a0c2d0
dwssd1250 <uuer248@gmail.com>
2024-04-09 17:07:36 +0900

    week 6

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class Welcome {
 	static final int NUM_BOOK = 3;
 	static final int NUM_ITEM = 7;
 	static CartItem[]mCartItem = new CartItem[NUM_BOOK];
 	static int mCartCount = 0;
+	static User mUser;
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -13,6 +15,8 @@ public class Welcome {
 		
 		System.out.print("연락처를 입력하세요: ");
 		int phone = input.nextInt();
+		
+		mUser = new User(name, phone);
 		
 		String[][]mBook = new String[NUM_BOOK][NUM_ITEM]; //BookList
 		
@@ -38,8 +42,8 @@ public class Welcome {
 			
 			System.out.println("메뉴 번호를 선택하세요: ");
 			int choice = input.nextInt();
-			if(choice < 1 || choice >8) {
-			System.out.println("1부터 8까지의 숫자를 입력하세요.");
+			if(choice < 1 || choice >9) {
+			System.out.println("1부터 9까지의 숫자를 입력하세요.");
 			}
 			else {
 				switch(choice) {
@@ -77,6 +81,10 @@ public class Welcome {
 					menuCartExit();
 					quit = true;
 					break;
+					
+				case 9:
+					menuAdminLogin();
+					break;
 				}
 			}//else
 			System.out.println("\n");
@@ -97,11 +105,13 @@ public class Welcome {
 		System.out.println("2. 장바구니 상품 목록 보기\t6. 장바구니의 항목 수량 줄이기");
 		System.out.println("3. 장바구니 비우기\t\t7. 장바구니의 항목 삭제하기");
 		System.out.println("4. 영수증 표시하기\t\t8. 종료");
+		System.out.println("9. 관리자 로그인");
 		System.out.println("****************************************");
 	}
 	public static void menuGuestInfo(String name, int phone) {
 		System.out.println("1. 현재 고객 정보: ");
-		System.out.println("이름: " + name + " 연락처: " + phone);
+		//System.out.println("이름: " + name + " 연락처: " + phone);
+		System.out.println("이름" + mUser.getName() + "  연락처" + mUser.getPhone());
 	}
 	public static void menuCartItemList() {
 		System.out.println("장바구니 상품 목록: ");
@@ -213,6 +223,26 @@ public class Welcome {
 			}
 		}
 		return flag;
+	}
+	
+	public static void menuAdminLogin() {
+		System.out.println("관리자 정보를 입력하세요");
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("아이디: ");
+		String adminId = input.next();
+		
+		System.out.print("비밀번호: ");
+		String adminPW = input.next();
+		
+		Admin admin = new Admin(mUser.getName(), mUser.getPhone());
+		if(adminId.equals(admin.getId()) && adminPW.equals(admin.getPassword())) {
+			System.out.println("이름 " + admin.getName() + "연락처 " + admin.getPhone());
+			System.out.println("아이디 " + admin.getId() + "비밀번호 " + admin.getPassword());
+		}
+		else {
+			System.out.println("관리자 정보가 일치하지 않습니다.");
+		}
 	}
 
 	
